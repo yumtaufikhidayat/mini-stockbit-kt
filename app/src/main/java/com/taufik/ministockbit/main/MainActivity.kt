7cc15eaa865547f51ef6ab52de9c54939ce3e811
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-08-14 11:34:02 +0700

    Updating login activity into login fragment and implementing login xml code into main activity

--- app/src/main/java/com/taufik/ministockbit/main/MainActivity.kt
+++ app/src/main/java/com/taufik/ministockbit/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.taufik.ministockbit
+package com.taufik.ministockbit.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/taufik/ministockbit/splashscreen/SplashscreenActivity.kt
+++ app/src/main/java/com/taufik/ministockbit/splashscreen/SplashscreenActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import com.taufik.ministockbit.MainActivity
 import com.taufik.ministockbit.databinding.ActivitySplashscreenBinding
+import com.taufik.ministockbit.main.MainActivity
 
 class SplashscreenActivity : AppCompatActivity() {
 
